0306359745b3e2f1060396bb967eccf1b723b10a
Oleksandra K <a.koroliova@gmail.com>
2024-06-21 15:12:03 +0300

    Remove comments

--- src/server.js
+++ src/server.js
@@ -19,12 +19,6 @@ export const setupServer = () => {
     }),
   );
 
-  // app.get('/', (req, res) => {
-  //   res.json({
-  //     message: 'Hello world!',
-  //   });
-  // });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
