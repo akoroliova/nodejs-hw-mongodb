8685458ef1f19627b7a8e0673fc5acece7c37b1c
Oleksandra K <a.koroliova@gmail.com>
2024-06-23 11:25:57 +0300

    Add error handling in getContactByIdController

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,15 +10,20 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
+  // if (!contact) {
+  //   res.status(404).json({
+  //     status: 404,
+  //     message: 'Contact not found',
+  //   });
+  //   return;
+  // }
+
   if (!contact) {
-    res.status(404).json({
-      status: 404,
-      message: 'Contact not found',
-    });
+    next(new Error('Contact not found'));
     return;
   }
 
