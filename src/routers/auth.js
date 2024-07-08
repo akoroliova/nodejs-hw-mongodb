e3a7a60a0a3ee210bb49e7444fe2627f84ff0c4a
Oleksandra K <a.koroliova@gmail.com>
2024-07-08 14:14:35 +0300

    Add user login logic

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema } from '../validation/auth-schemas.js';
-import { registerUserController } from '../controllers/auth.js';
 import validateBody from '../middlewares/validateBody.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+} from '../validation/auth-schemas.js';
+import {
+  registerUserController,
+  loginUserController,
+} from '../controllers/auth.js';
 
 const authRouter = Router();
 
@@ -10,6 +16,12 @@ authRouter.post(
   '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
 );
 
 export default authRouter;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
 import { User } from '../db/models/User.js';
 import createHttpError from 'http-errors';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { Session } from '../db/models/Session.js';
 
 export const registerUser = async (payload) => {
   const user = await User.findOne({ email: payload.email });
@@ -11,5 +14,28 @@ export const registerUser = async (payload) => {
   return await User.create({
     ...payload,
     password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (payload) => {
+  const user = await User.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+  await Session.deleteOne({ userId: user._id });
+  return await Session.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
 };
